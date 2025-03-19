94425cc6ea60bf31f316195f6a1dce96dab23781
timboSlice-k <timkwan44@gmail.com>
2025-03-19 01:55:03 -0700

    more change

--- optimization223.m
+++ optimization223.m
@@ -4,10 +4,16 @@ angvel = 559.0987726;
 gearratio = 10:1:200;
 rwheel = 0.011;
 
+hillAng = atan(0.2/0.5);
 
-maxcargo = motors * gearratio * sin(hillAng)/rwheel; 
+G = 550;
+
+
+maxcargo = motors * gearratio * sin(hillAng)/rwheel;
+M_w = 1-0.5*cos(0.5*pi*maxcargo/max(maxcargo));
 
 speed = angvel * rwheel * gearratio.^-1 ;
+B_1 = 250-250*cos(pi*speed/max(speed));
 
 mass = 0.15; %in kg
 rwheel = 0.011; %in m
@@ -15,11 +21,8 @@ mtorque = motors * 0.008647; %Nm
 
 fric = 0.64; %static mu between wheels and track
 
-%want to maximize 5 * speed * cargo - 10 * mass - motors
+score = (B_1 + 2 * G) * M_w^2;
 
-
-
-hillAng = atan(0.2/0.5);
 
 
 
