54f1ee9913be78cd040171b86d16c32e47186f49
timboSlice-k <timkwan44@gmail.com>
2025-03-19 00:31:40 -0700

    changed some parameters

--- optimization223.m
+++ optimization223.m
@@ -1,10 +1,26 @@
 motors = 1;
-batteries = motors * 4;
+angvel = 559.0987726;
+
+gearratio = 10:1:200;
+rwheel = 0.011;
+
+
+maxcargo = motors * gearratio * sin(hillAng)/rwheel; 
+
+speed = angvel * rwheel * gearratio.^-1 ;
+
 mass = 0.15; %in kg
+rwheel = 0.011; %in m
+mtorque = motors * 0.008647; %Nm
+
 fric = 0.64; %static mu between wheels and track
 
 %want to maximize 5 * speed * cargo - 10 * mass - motors
 
-f = @(s, c, m, M) 5*s*c - 10*m - M;
+
 
 hillAng = atan(0.2/0.5);
+
+
+
+
